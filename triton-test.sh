7905ccf91fc0f46b79beebd9cbd8181b5d74de1e
okn <none@none>
2019-04-25 11:14:44 +0300

    changes to triton environment

--- triton-test.sh
+++ triton-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=0-00:05:00		# 5 mins
+#SBATCH --time=0-00:15:00		# 15 mins
 #SBATCH --mem=10G     			# 10GB of memory
 #SBATCH --gres=gpu:1			# one GPU
 #SBATCH -c 2					# two CPUs
@@ -7,4 +7,5 @@
 
 cd $WRKDIR/densepose/src
 module load anaconda3
-srun python3 main.py -a densenet121 -o adam --lr 1e-4 --wd 0.5 -b 64 --epochs 5
+source activate $WRKDIR/conda/densepose
+srun python3 main.py -d $WRKDIR/data/cambridge -a densenet121 -o adam --lr 1e-4 --wd 0.5 -b 64 --epochs 1
